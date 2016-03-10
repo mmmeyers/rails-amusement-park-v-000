761bdae0758912f7693c3b1221280a071fde6fa9
mmmeyers <marsi286@gmail.com>
2016-03-10 14:31:56 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'welcome#home'
 
+  get '/users/new', to: 'users#new'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
 
+  resources :users
+  resources :attractions
+  resources :rides
 end
